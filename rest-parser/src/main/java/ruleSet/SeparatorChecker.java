067220379f311ac2bb244fa679cfc0bbb82fbd79
Timmy58 <tidee98@web.de>
2022-07-18 15:29:20 +0200

    added to golden standard plus started rule implementation
    Former-commit-id: 1bfed9aa06d6695f44f1e686e1263af61a4e163b [formerly 299b8b24cc8888437980496d8ae60b0597274dd3]
    Former-commit-id: e37f8b3be207ea8d76d7b501f87797513710edf4
    Former-commit-id: 405b40026735a22e8a89146b5520cdd0db6b2225

--- rest-parser/src/main/java/ruleSet/SeparatorChecker.java
+++ rest-parser/src/main/java/ruleSet/SeparatorChecker.java
@@ -1,7 +1,38 @@
 package ruleSet;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * RULE: Forward slash separator (/) must be used to indicate a hierarchical relationship
  */
 public class SeparatorChecker {
+    private static char[] separators = { '-', '.', ':', ';', ',', '\\','#'};
+    //TODO:
+    //check expected pattern
+    //check for other possible separators such as '-', '.', ':', ';', ',', '\', '#'
+    //and count their appearances
+    // at a threshhold: generate antipattern
+    //http://regexr.com/?37i6s
+    public static boolean checkSeparator(List<String> uriList){
+        //expected Pattern
+        Pattern expectedPattern = Pattern.compile("(.* /)*");
+
+        for (String url: uriList) {
+            try {
+                URL urlObject = new URL(url);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+            //count appearance
+            for (int i=0; i< separators.length; i++) {
+                char separator = separators[i];
+                long num = url.chars().filter(ch -> ch == separator).count();
+            }
+
+        }
+        return false;
+    }
 }
