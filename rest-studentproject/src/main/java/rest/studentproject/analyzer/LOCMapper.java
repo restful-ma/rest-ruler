7182d63f866da205e3895ba94b32e44c42831d6e
manuelmerkel <manuel.merkel98@gmail.com>
2022-07-19 11:05:32 +0200

    code: add enums; updated rule interface; added underscore rule function

---
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/LOCMapper.java
@@ -0,0 +1,40 @@
+package rest.studentproject.analyzer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Map;
+
+public class LOCMapper {
+    public Map<String, Integer> keyLOCMap;
+    private String path = "";
+    private OpenAPI openAPI = null;
+    public List<String> openAPIKeys;
+
+    public LOCMapper(String path, OpenAPI openAPI) {
+        this.path = path;
+        this.openAPI = openAPI;
+
+        getOpenAPIKeys();
+        mapOpenAPIKeysToLOC();
+
+    }
+
+    private Array getOpenAPIKeys() {
+        // Alle wichtigen keys erhalten. Beachte: OpenAPI 3.0 und 2.0 haben verschiedene keys
+        return null;
+    }
+
+    private void mapOpenAPIKeysToLOC() {
+        // Durchsuche file nach String keys aus getOpenAPIKeys()
+    }
+
+    public Map<String, Integer> getOpenAPIKeyLOC() {
+        return this.keyLOCMap;
+    }
+
+    public int getLOCKey(String key) {
+        return this.keyLOCMap.get(key);
+    }
+}
--- rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
@@ -1,12 +1,10 @@
 package rest.studentproject.analyzer;
 
 import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import rest.studentproject.report.Report;
-import rest.studentproject.rules.LowercaseRule;
-
-import java.util.ArrayList;
-import java.util.List;
+import rest.studentproject.rules.UnderscoreRule;
 
 public class RestAnalyzer {
 
@@ -15,19 +13,18 @@ public class RestAnalyzer {
         // List<Report> reportList = new ArrayList<>();
         Report finalReport = new Report();
         SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation(url, null, null);
+        OpenAPI openAPI = swaggerParseResult.getOpenAPI();
 
+//        LOCMapper locMapper = new LOCMapper(url, openAPI);
 
-        // foreach ( ) { }
-        LowercaseRule lowercaseRule = new LowercaseRule();
-        Report report = lowercaseRule.checkViolation(swaggerParseResult);
-        // reportList.add(report);
-        // report.id report.description ...  x49
-
+        // Iterates over all active rules
+        UnderscoreRule underscoreRule = new UnderscoreRule();
+        underscoreRule.checkViolation(openAPI);
 
 
         // finalReport.generateReport(reportList);
 
 
-        return report;
+        return finalReport;
     }
 }
