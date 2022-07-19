e68fd11fe862dff0adfbd12a60f8f1cd6df46549
Timmy58 <tidee98@web.de>
2022-07-19 15:57:21 +0200

    implemented Rule interface
    Former-commit-id: 5818ca078f78051d234a8a2223a9a0c1b3ce9c8b [formerly 53b109582cf7d3feaaf92ab2ec3461caac451a54]
    Former-commit-id: dbfa99297336caa1db313daa94682d8335faf465
    Former-commit-id: c4f4465fe5b7a425a4f9916eaa9531ab53731db1

--- rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
@@ -4,7 +4,11 @@ import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import rest.studentproject.report.Report;
+import rest.studentproject.rules.SeparatorChecker;
 import rest.studentproject.rules.UnderscoreRule;
+import rest.studentproject.rules.Violation;
+
+import java.util.List;
 
 public class RestAnalyzer {
 
@@ -19,6 +23,14 @@ public class RestAnalyzer {
         UnderscoreRule underscoreRule = new UnderscoreRule(true);
         // TODO: Handle returned violation list
         underscoreRule.checkViolation(openAPI);
+
+
+        SeparatorChecker separatorChecker = new SeparatorChecker();
+        List<Violation> violations = separatorChecker.checkViolation(openAPI);
+
+        for (Violation v: violations) {
+            System.out.println(v.getErrorMessage());
+        }
 
         return finalReport;
     }
