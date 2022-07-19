769fe23c16b0d12372e8dd71cab0056305aeac75
Timmy58 <tidee98@web.de>
2022-07-19 14:18:07 +0200

    implementation of first draft of regex for rule19
    Former-commit-id: 7f5e270a946bba7860f3e526d2da4b18675ee284 [formerly d79e6a2c6c85c5fcb5e316ccb6c22941163d23e3]
    Former-commit-id: 543876db805a9b997c75db3d889d72badd1494a8
    Former-commit-id: 87e222efab8e0d0378e6dcaf42050fef4e71995c

--- rest-parser/src/main/java/ruleSet/SeparatorChecker.java
+++ rest-parser/src/main/java/ruleSet/SeparatorChecker.java
@@ -1,38 +1,97 @@
 package ruleSet;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * RULE: Forward slash separator (/) must be used to indicate a hierarchical relationship
  */
 public class SeparatorChecker {
-    private static char[] separators = { '-', '.', ':', ';', ',', '\\','#'};
+    private static char[] separators = { '-', '.', ':', ';', ',', '\\','#', '/'};
     //TODO:
     //check expected pattern
     //check for other possible separators such as '-', '.', ':', ';', ',', '\', '#'
     //and count their appearances
     // at a threshhold: generate antipattern
     //http://regexr.com/?37i6s
-    public static boolean checkSeparator(List<String> uriList){
+
+    /**
+     * checks a given path for potential rule violations
+     * @param urlList
+     * @return
+     */
+    public static boolean checkSeparator(List<String> urlList){
         //expected Pattern
-        Pattern expectedPattern = Pattern.compile("(.* /)*");
+        Pattern expectedPattern = Pattern.compile("^(\\/((\\{\\d+\\})|[-a-zA-Z0-9@:%_\\+.~#?&=]+))+$");
 
-        for (String url: uriList) {
-            try {
-                URL urlObject = new URL(url);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-            //count appearance
-            for (int i=0; i< separators.length; i++) {
-                char separator = separators[i];
-                long num = url.chars().filter(ch -> ch == separator).count();
-            }
+        for (String url: urlList) {
 
+            Matcher matcher = expectedPattern.matcher(url);
+            if (matcher.find()){
+                //check....
+            }else {
+                //check if a different character is used as a separator
+                Map<Character, Long> appearances = countPossibleSeparators(url);
+                long ForwardSlashAppearances = appearances.get('/');
+
+                for (char c: appearances.keySet()) {
+                    if (appearances.get(c) >= ForwardSlashAppearances && c != '/'){
+                        boolean isSeparator = checkForSeparator(c, url);
+                        if (isSeparator){
+                            //TODO: creat violation object
+                        }
+                    }
+                }
+                //return false;
+            }
         }
         return false;
+    }
+
+    /**
+     * checks a String for possible separators
+     * @param separator separator candidate
+     * @param path path under test
+     * @return true or fals deoending on if it is a separator
+     */
+    private static boolean checkForSeparator(char separator, String path){
+
+        Pattern pattern;
+
+        //escape regex operation characters
+        switch (separator){
+            case '.':
+                pattern = Pattern.compile("^(" + "\\." + "((\\{\\d+\\})|[-a-zA-Z0-9@:%_\\+.~#?&=]+))+$");
+                break;
+            case '\\':
+                pattern = Pattern.compile("^(" + "\\\\" + "((\\{\\d+\\})|[-a-zA-Z0-9@:%_\\+.~#?&=]+))+$");
+                break;
+            default:
+                pattern = Pattern.compile("^(" + separator + "((\\{\\d+\\})|[-a-zA-Z0-9@:%_\\+.~#?&=]+))+$");
+        }
+
+        Matcher matcher = pattern.matcher(path);
+
+        return matcher.find();
+    }
+
+    /**
+     * counts all appearances of possible separator characters
+     * @param url
+     * @return
+     */
+    private static Map<Character, Long> countPossibleSeparators(String url){
+
+        HashMap<Character, Long> appearances = new HashMap<>();
+        //count appearance
+        for (int i=0; i< separators.length; i++) {
+            char separator = separators[i];
+            long num = url.chars().filter(ch -> ch == separator).count();
+            appearances.put(separator,num);
+        }
+        return appearances;
     }
 }
