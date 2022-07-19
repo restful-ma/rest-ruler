9aedf4c95416275c26db2acad20d0e654c49b4c8
Timmy58 <tidee98@web.de>
2022-07-19 15:57:21 +0200

    implemented Rule interface

--- rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
@@ -4,7 +4,11 @@ import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import rest.studentproject.report.Report;
+import rest.studentproject.rules.SeparatorChecker;
 import rest.studentproject.rules.UnderscoreRule;
+import rest.studentproject.rules.Violation;
+
+import java.util.List;
 
 public class RestAnalyzer {
 
@@ -18,9 +22,15 @@ public class RestAnalyzer {
 //        LOCMapper locMapper = new LOCMapper(url, openAPI);
 
         // Iterates over all active rules
-        UnderscoreRule underscoreRule = new UnderscoreRule(true);
-        underscoreRule.checkViolation(openAPI);
+        //UnderscoreRule underscoreRule = new UnderscoreRule(true);
+        //underscoreRule.checkViolation(openAPI);
 
+        SeparatorChecker separatorChecker = new SeparatorChecker();
+        List<Violation> violations = separatorChecker.checkViolation(openAPI);
+
+        for (Violation v: violations) {
+            System.out.println(v.getErrorMessage());
+        }
 
         // finalReport.generateReport(reportList);
 
