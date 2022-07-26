745f370883c4dd3c60e3992f39cef67e5eed13cb
manuelmerkel <manuel.merkel98@gmail.com>
2022-07-26 15:37:22 +0200

    feature: add mapper between paths and LOC

--- rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
@@ -4,29 +4,29 @@ import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import rest.studentproject.report.Report;
-import rest.studentproject.rules.LowercaseRule;
 import rest.studentproject.rules.UnderscoreRule;
+import rest.studentproject.rules.Violation;
+
+import java.net.MalformedURLException;
 
 public class RestAnalyzer {
 
-
-    public Report runAnalyse(String url){
-        // List<Report> reportList = new ArrayList<>();
+    public Report runAnalyse(String url) throws MalformedURLException {
         Report finalReport = new Report();
         SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation(url, null, null);
         OpenAPI openAPI = swaggerParseResult.getOpenAPI();
 
-//        LOCMapper locMapper = new LOCMapper(url, openAPI);
+        LOCMapper locMapper = new LOCMapper(openAPI, url);
 
         // Iterates over all active rules
-        // UnderscoreRule underscoreRule = new UnderscoreRule(true);
-        // underscoreRule.checkViolation(openAPI);
-        LowercaseRule lowercaseRule = new LowercaseRule(true);
-        lowercaseRule.checkViolation(openAPI);
-
-
-
-        // finalReport.generateReport(reportList);
+        UnderscoreRule underscoreRule = new UnderscoreRule(true, locMapper);
+        // TODO: Handle returned violation list
+        for (Violation violation : underscoreRule.checkViolation(openAPI)){
+            System.out.println(violation.getLineViolation());
+            System.out.println(violation.getKeyViolation());
+            System.out.println(violation.getErrorMessage());
+            System.out.println(violation.getImprovementSuggestion());
+        }
 
 
         return finalReport;
--- rest-studentproject/src/main/java/rest/studentproject/rules/IRestRule.java
+++ rest-studentproject/src/main/java/rest/studentproject/rules/IRestRule.java
@@ -1,10 +1,10 @@
 package rest.studentproject.rules;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import rest.studentproject.rules.attributes.RuleCategory;
-import rest.studentproject.rules.attributes.RuleSeverity;
-import rest.studentproject.rules.attributes.RuleSoftwareQualityAttribute;
-import rest.studentproject.rules.attributes.RuleType;
+import rest.studentproject.rules.constants.RuleCategory;
+import rest.studentproject.rules.constants.RuleSeverity;
+import rest.studentproject.rules.constants.RuleSoftwareQualityAttribute;
+import rest.studentproject.rules.constants.RuleType;
 
 import java.util.List;
 
