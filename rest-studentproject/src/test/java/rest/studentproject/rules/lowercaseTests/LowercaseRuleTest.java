4238b5536f428ede0b08d6c02beca07fc1aa82b9
Daniel Abajirov <abajirovdaniel@gmail.com>
2022-07-26 11:32:04 +0200

    fix: fix some sonarlint issue

--- rest-studentproject/src/test/java/rest/studentproject/rules/lowercaseTests/LowercaseRuleTest.java
+++ rest-studentproject/src/test/java/rest/studentproject/rules/lowercaseTests/LowercaseRuleTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LowercaseRuleTest {
+class LowercaseRuleTest {
     LowercaseRule lowercaseRule;
 
     @BeforeEach
