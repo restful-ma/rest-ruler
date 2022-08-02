d60642acb3dc9af0d9db3e554185474af7438f0b
manuelmerkel <manuel.merkel98@gmail.com>
2022-08-02 16:01:39 +0200

    fix: format code
    Former-commit-id: b9f1c6c40656891961e0493a59fda131edaacb00 [formerly 42966b41eab55c93d72635fd524a5c6a43f7f700]
    Former-commit-id: bcc46e7507f3b7216057290a0f3eb567488d264c
    Former-commit-id: 08b51867414373361d37d370e9297cc05b386d79

--- rest-studentproject/src/test/java/rest/studentproject/rules/underscoretest/UnderscoreRuleTest.java
+++ rest-studentproject/src/test/java/rest/studentproject/rules/underscoretest/UnderscoreRuleTest.java
@@ -1,8 +1,5 @@
 package rest.studentproject.rules.underscoretest;
 
-import io.swagger.parser.OpenAPIParser;
-import io.swagger.v3.oas.models.OpenAPI;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import rest.studentproject.analyzer.RestAnalyzer;
@@ -12,7 +9,7 @@ import rest.studentproject.rules.Violation;
 import java.net.MalformedURLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UnderscoreRuleTest {
 
@@ -28,14 +25,12 @@ class UnderscoreRuleTest {
         assertEquals(0, violations.get(0).size(), "There should be no rule violation for the valid openAPI definition.");
     }
 
+
     @Test
     void invalidFile() throws MalformedURLException {
         this.restAnalyzer = new RestAnalyzer("src/test/java/rest/studentproject/rules/underscoretest/InvalidOpenAPI.json");
         this.underscore = new UnderscoreRule(true);
         List<List<Violation>> violations = this.restAnalyzer.runAnalyse(List.of(this.underscore));
         assertEquals(3, violations.get(0).size(), "There should be three rule violations. Three of the paths are valid.");
-
     }
-
-
 }
