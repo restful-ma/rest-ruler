0220b0e8e1af1b7c2f5b04e400df21b14ff83e40
daniel.abajirov <abajirovdaniel@gmail.com>
2022-08-07 23:12:01 +0200

    fix: fix merge conflicts. Update tests

--- rest-studentproject/src/test/java/rest/studentproject/rules/hyphensTests/HyphensRuleTest.java
+++ rest-studentproject/src/test/java/rest/studentproject/rules/hyphensTests/HyphensRuleTest.java
@@ -6,17 +6,20 @@ import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import rest.studentproject.analyzer.RestAnalyzer;
 import rest.studentproject.rules.HyphensRule;
 import rest.studentproject.rules.LowercaseRule;
 import rest.studentproject.rules.Violation;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HyphensRuleTest {
     HyphensRule hyphensRuleTest;
+    RestAnalyzer restAnalyzer;
 
     @BeforeEach
     void setUp() {
@@ -25,46 +28,37 @@ public class HyphensRuleTest {
     }
     @Test
     @DisplayName("Detect if a path segment contains more than a word. Here 6 paths contain a violation.")
-    void checkViolationOnInvalidRESTFile2Violations() {
-        SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid2Violations.json", null, null);
-        OpenAPI openAPI = swaggerParseResult.getOpenAPI();
-        List<Violation> violationToTest = null;
-        try {
-            violationToTest = hyphensRuleTest.checkViolation(openAPI);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        assertEquals(2, violationToTest.size(),
+    void checkViolationOnInvalidRESTFile2Violations() throws MalformedURLException {
+        this.restAnalyzer = new RestAnalyzer("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid2Violations.json");
+        List<List<Violation>> violationToTest = this.restAnalyzer.runAnalyse(List.of(this.hyphensRuleTest));
+        assertEquals(2, violationToTest.get(0).size(),
                 "Detection of violations should work.");
     }
 
     @Test
     @DisplayName("Detect if a path segment contains more than a word. Here 2 paths contain a violation.")
-    void checkViolationOnInvalidRESTFile2ViolationsLowercase() {
-        SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid2ViolationsLowercase.json", null, null);
-        OpenAPI openAPI = swaggerParseResult.getOpenAPI();
-        List<Violation> violationToTest = null;
-        try {
-            violationToTest = hyphensRuleTest.checkViolation(openAPI);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        assertEquals(2, violationToTest.size(),
+    void checkViolationOnInvalidRESTFile2ViolationsLowercase() throws MalformedURLException {
+        this.restAnalyzer = new RestAnalyzer("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid2ViolationsLowercase.json");
+        List<List<Violation>> violationToTest = this.restAnalyzer.runAnalyse(List.of(this.hyphensRuleTest));;
+        assertEquals(2, violationToTest.get(0).size(),
                 "Detection of violations should work.");
     }
 
     @Test
     @DisplayName("Detect if a path segment contains more than a word. Here 9 paths contain a violation.")
-    void checkViolationOnInvalidRESTFile6Violations() {
-        SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid9ViolationsSpecialCharactersLowercase.json", null, null);
-        OpenAPI openAPI = swaggerParseResult.getOpenAPI();
-        List<Violation> violationToTest = null;
-        try {
-            violationToTest = hyphensRuleTest.checkViolation(openAPI);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        assertEquals(9, violationToTest.size(),
+    void checkViolationOnInvalidRESTFile6Violations() throws MalformedURLException {
+        this.restAnalyzer = new RestAnalyzer("src/test/java/rest/studentproject/rules/hyphensTests/hyphensInvalid9ViolationsSpecialCharactersLowercase.json");
+        List<List<Violation>> violationToTest = this.restAnalyzer.runAnalyse(List.of(this.hyphensRuleTest));;
+        assertEquals(9, violationToTest.get(0).size(),
+                "Detection of violations should work.");
+    }
+
+    @Test
+    @DisplayName("Detect if a path segment contains more than a word. Here 9 paths contain a violation.")
+    void checkViolationOnValidRESTFile() throws MalformedURLException {
+        this.restAnalyzer = new RestAnalyzer("src/test/java/rest/studentproject/validopenapi/validOpenAPI.json");
+        List<List<Violation>> violationToTest = this.restAnalyzer.runAnalyse(List.of(this.hyphensRuleTest));;
+        assertEquals(0, violationToTest.get(0).size(),
                 "Detection of violations should work.");
     }
 }
