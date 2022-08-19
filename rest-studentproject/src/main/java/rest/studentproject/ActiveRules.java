562ba705318ac500a64d9865c846168ac9b893c5
manuelmerkel <manuel.merkel98@gmail.com>
2022-08-19 17:24:30 +0200

    feat: add config file
    Former-commit-id: e622441c40f3aab9fed73eee3173239aef0b990e [formerly 6412965949302a696202f089e9f7372e65d79d2b]
    Former-commit-id: a2e712798157920aa1fd1e9b8cb6cd80d4f3ecdd
    Former-commit-id: 99a1d4948087f7eeac77ecded62e61e2d4dbe656

--- rest-studentproject/src/main/java/rest/studentproject/ActiveRules.java
+++ rest-studentproject/src/main/java/rest/studentproject/ActiveRules.java
@@ -2,13 +2,20 @@ package rest.studentproject;
 
 import rest.studentproject.rules.*;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
- * All rules that are implemented need to be added here in this class. If the rule should be analyzed the rule object needs as parameter true.
+ * All rules that are implemented need to be added here in this class. If the rule should be analyzed the rule object
+ * needs as parameter true.
  */
 public class ActiveRules {
+    private static final String CONFIG_FILE_PATH = "src\\main" + "\\java\\rest\\studentproject\\docs\\config.properties";
+    private Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     /**
      * Add all rule objects here that are implemented
@@ -16,6 +23,46 @@ public class ActiveRules {
      * @return the list of implemented rules
      */
     public List<IRestRule> getActiveRules() {
-        return Arrays.asList(new UnderscoreRule(true), new CRUDRule(true), new LowercaseRule(true), new HyphensRule(true), new SeparatorRule(true));
+        Properties prop = getConfig();
+
+        if (prop == null)
+            return Arrays.asList(new UnderscoreRule(true), new CRUDRule(false), new LowercaseRule(true),
+                    new HyphensRule(true), new SeparatorRule(true));
+
+        return Arrays.asList(new UnderscoreRule(Boolean.parseBoolean(prop.getProperty("Underscores(_)" +
+                "shouldnotbeusedinURI"))), new CRUDRule(Boolean.parseBoolean(prop.getProperty(
+                        "CRUDfunctionnamesshouldnotbeusedinURIs"))),
+                new LowercaseRule(Boolean.parseBoolean(prop.getProperty("LowercaselettersshouldbepreferredinURIpaths"))), new HyphensRule(Boolean.parseBoolean(prop.getProperty("Hyphens(-)shouldbeusedtoimprovethereadabilityofURIs"))), new SeparatorRule(Boolean.parseBoolean(prop.getProperty("Forwardslashseparator(/)mustbeusedtoindicateahierarchicalrelationship"))));
+    }
+
+    public void setActiveRules(Map<String, Boolean> ruleSet) {
+        Properties prop = getConfig();
+
+        if (prop == null || ruleSet == null) return;
+
+        try {
+            FileOutputStream out = new FileOutputStream(CONFIG_FILE_PATH);
+            for (Map.Entry<String, Boolean> rule : ruleSet.entrySet()) {
+                prop.setProperty(rule.getKey(), rule.getValue().toString());
+            }
+            prop.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            logger.severe("Exception" + e.getMessage());
+        }
+    }
+
+    private Properties getConfig() {
+        FileInputStream propsInput = null;
+        try {
+            propsInput = new FileInputStream(CONFIG_FILE_PATH);
+            Properties prop = new Properties();
+            prop.load(propsInput);
+            return prop;
+        } catch (IOException e) {
+            logger.severe("Error when trying to read the config file: " + e.getMessage());
+            logger.info("Instead using the default values --> all rules are active.");
+            return null;
+        }
     }
 }
