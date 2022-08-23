f77f7be7910c10a90428067ef09dd3cda7619e90
Timothy <tidee98@web.de>
2022-08-23 16:49:28 +0200

    feature: Implemented Rule, added Test files and JUnit Testcases

--- rest-studentproject/src/main/java/rest/studentproject/rules/constants/ErrorMessage.java
+++ rest-studentproject/src/main/java/rest/studentproject/rules/constants/ErrorMessage.java
@@ -6,6 +6,13 @@ public class ErrorMessage {
     public static final String LOWERCASE = "Lowercase letters should be preferred in URI paths";
     public static final String SEPARATOR = "A forward slash '/' has to be used as a separator";
     public static final String HYPHEN = "Hyphens (-) should be used to improve the readability of URIs";
-    public static final String GET_RESOURCE = "";
+    public static final String GET_RESOURCE = "GET request is missing a content definition for its response";
+    public static final String GET_RESOURCE_REQUESTBODY = "GET requests do not have a request body";
+    public static final String GET_RESOURCE_MISSING_RESPONSE = "Missing Response";
+
+
+    private ErrorMessage(){
+        throw new IllegalStateException("Utility class");
+    }
 
 }
