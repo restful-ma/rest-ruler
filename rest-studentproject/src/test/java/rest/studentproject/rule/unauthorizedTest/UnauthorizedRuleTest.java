8a1caa80d232c7e3fe9e9cee0667d32e11187415
st155131 <st155131@stud.uni-stuttgart.de>
2022-09-02 18:43:40 +0200

    test: add 401 test without function

---
+++ rest-studentproject/src/test/java/rest/studentproject/rule/unauthorizedTest/UnauthorizedRuleTest.java
@@ -0,0 +1,18 @@
+package rest.studentproject.rule.unauthorizedTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnauthorizedRuleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+}
