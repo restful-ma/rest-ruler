2d181c8ad81b2565702f4c8027578bccd0fdc0bd
st155131 <st155131@stud.uni-stuttgart.de>
2022-09-04 16:50:57 +0200

    test: add tests for unauthorized rule
    Former-commit-id: 1706495ace888cdd9a74d4393508caddf3c5b1e1 [formerly c4a01416e34571038ac46c2a92a01eb6f6d3f457]
    Former-commit-id: 4f6444bb2b9030b53bbbee75dac1505bff1d30c1
    Former-commit-id: 1812a435cd3320ab1b560a8a0178e6e10cc6728f

--- rest-studentproject/src/test/java/rest/studentproject/rule/unauthorizedTest/UnauthorizedRuleTest.java
+++ rest-studentproject/src/test/java/rest/studentproject/rule/unauthorizedTest/UnauthorizedRuleTest.java
@@ -1,18 +1,46 @@
 package rest.studentproject.rule.unauthorizedTest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import rest.studentproject.analyzer.RestAnalyzer;
+import rest.studentproject.rule.Violation;
+import rest.studentproject.rule.rules.UnauthorizedRule;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnauthorizedRuleTest {
+    final static String PATH_GLOBAL_SEC = "src/test/java/rest/studentproject/rule/unauthorizedTest" +
+            "/InvalidOpernAPIUnauthorizedRuleGlobalSec.json";
+    final static String PATH_LOCAL_SEC = "src/test/java/rest/studentproject/rule/unauthorizedTest" +
+            "/InvalidOpenAPIUnauthorizedRuleLocalSec.json";
+    final static String PATH_TO_VALIDOPEAPI = "src/test/java/rest/studentproject/validopenapi/validOpenAPI.json";
+    final static List<String> PATHS_TO_OPENAPI = List.of(PATH_GLOBAL_SEC, PATH_LOCAL_SEC);
 
-    @BeforeEach
-    void setUp() {
+    RestAnalyzer restAnalyzer;
+    UnauthorizedRule unauthorizedRule;
+
+    @Test
+    @DisplayName("Test that checks if the rule violation is detected for globally and locally defined security.")
+    void checkGlobalAndLocalSec() {
+        for (String path : PATHS_TO_OPENAPI) {
+            List<Violation> violations = runMethodUnderTest(path);
+            assertEquals(3, violations.size());
+        }
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("Test that checks if there is no rule violation for the valid OpenAPI definition.")
+    void checkValidOpenAPI() {
+        List<Violation> violations = runMethodUnderTest(PATH_TO_VALIDOPEAPI);
+        assertTrue(violations.isEmpty());
     }
 
+    private List<Violation> runMethodUnderTest(String path) {
+        this.restAnalyzer = new RestAnalyzer(path);
+        this.unauthorizedRule = new UnauthorizedRule(true);
+        return new ArrayList<>(this.restAnalyzer.runAnalyse(List.of(this.unauthorizedRule), false));
+    }
 }
