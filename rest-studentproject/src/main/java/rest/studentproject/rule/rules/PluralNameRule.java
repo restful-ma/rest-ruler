36fd1267091001c65a85b30c081093e2487b3da1
daniel.abajirov <abajirovdaniel@gmail.com>
2022-09-06 16:31:31 +0200

    fix: fix read from json
    Former-commit-id: 82312befbaa3b8fc6a40bdcb7e2bbe09b8577192 [formerly 3fc8d460292884cf3d73252361567e86f1f40faa]
    Former-commit-id: e50b6869bda14cc51df20886abe558d10d9f0122
    Former-commit-id: b73237b3bddcefd1ff66d00289ac6cb3c5fe1b79

--- rest-studentproject/src/main/java/rest/studentproject/rule/rules/PluralNameRule.java
+++ rest-studentproject/src/main/java/rest/studentproject/rule/rules/PluralNameRule.java
@@ -119,7 +119,11 @@ public class PluralNameRule implements IRestRule {
             // If the word is contained and the form of the word doesn't match the current switchPathSegment then we have a violation
             if (Boolean.TRUE.equals(isPathSegmentInJson.getLeft()) && switchPathSegment.equals(isPathSegmentInJson.getRight()) && switchPathSegment.equals(SINGULAR) && !listPathSegments.get(0).equals(pathSegment)) {
                 return new Violation(this, locMapper.getLOCOfPath(path), ImprovementSuggestion.PLURALNAME, path, ErrorMessage.PLURALNAME+ WITH_PATH_SEGMENT + pathSegment);
-            }else if(Boolean.TRUE.equals(isPathSegmentInJson.getLeft())) continue;
+            }else if(Boolean.TRUE.equals(isPathSegmentInJson.getLeft())) {
+                switchPathSegment = skipFirstSwitchPathSegmentAssign ? switchPathSegment : getControlPathSegmentForRule(switchPathSegment.equals(PLURAL));
+                skipFirstSwitchPathSegmentAssign = false;
+                continue;
+            }
 
             // If the word is not contained in the jsonDictionary, we need to check if the word is present in the OxfordDictionaryAPI
             // We get a true if the word is singular otherwise a false if it is plural
