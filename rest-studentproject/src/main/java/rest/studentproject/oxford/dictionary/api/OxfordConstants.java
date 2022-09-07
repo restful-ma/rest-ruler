1e1e05ea1ad7df90b2221ccda7ed2ca5de0ce688
st155131 <st155131@stud.uni-stuttgart.de>
2022-09-07 14:29:16 +0200

    feat: Input for auth
    Former-commit-id: a8c0b9b1a0459a854cf6300241573e820bb3124e [formerly 0f3935819c3c43114d14d50c2a8da20105a04ceb]
    Former-commit-id: 65a848a4dc9a9c7e119bd178a1842ed47921ecc4
    Former-commit-id: 1b16f1d083e4e1f50167427c0e87f022dc098c26

--- rest-studentproject/src/main/java/rest/studentproject/oxford/dictionary/api/OxfordConstants.java
+++ rest-studentproject/src/main/java/rest/studentproject/oxford/dictionary/api/OxfordConstants.java
@@ -9,6 +9,6 @@ public class OxfordConstants {
     public static final String PLURAL = "plural";
     public static final String SINGULAR = "singular";
     public static final String REST_URL = "https://od-api.oxforddictionaries.com:443/api/v2/words/en-gb?q=";
-    public static final String APP_ID = "d5a1a02e";
-    public static final String APP_KEY = "de67c34bbab051ef34f729b2e952a51a";
+    public static final String APP_ID = "b079a73e";
+    public static final String APP_KEY = "1f311912a89d60e09a041af7b191155a";
 }
