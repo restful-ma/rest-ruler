b249df231370fec27b72f62ed13f08e45409d144
Timothy <tidee98@web.de>
2022-10-08 16:38:58 +0200

    added new parameter option in commandline execution + commenting code
    Former-commit-id: 09f970d6288860dfad5fd22e22370e49b2ef308d [formerly fe6ef809edf1b49888e0bb4877cb3062e77f2ef0]
    Former-commit-id: 5d24c9f5793bd7344018c2cfae83a6f77b33c3bd
    Former-commit-id: 744c1d777f494ae0c49a7c10c4e76a8f724f3a75 [formerly 24825a08875b6f16f7155f56f53b9179df115d41]
    Former-commit-id: 8aa659f574ef8c5fb321ed2b8aa4ccb0925d86f6

--- rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
+++ rest-studentproject/src/main/java/rest/studentproject/analyzer/RestAnalyzer.java
@@ -12,12 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RestAnalyzer {
-    public static LOCMapper locMapper = null;
+    public static LOCMapper locMapper;
     private final OpenAPI openAPI;
     //Singleton
     private static final Report report = Report.getInstance();
 
 
+    /**
+     * Constructor
+     * @param url location of the OpenAPI file (link or file path)
+     */
     public RestAnalyzer(String url) {
         SwaggerParseResult swaggerParseResult = new OpenAPIParser().readLocation(url, null, null);
         this.openAPI = swaggerParseResult.getOpenAPI();
@@ -26,16 +30,53 @@ public class RestAnalyzer {
     }
 
 
+    /**
+     * executes rule checking analysis for provided list of rules. Optionally generates a report file.
+     * returns a list of all violations found
+     * @param activeRules all rules to be checked for
+     * @param generateReport generates report if boolean value true
+     * @return list of all violation objects
+     */
     public List<Violation> runAnalyse(List<IRestRule> activeRules, boolean generateReport) {
-        List<Violation> violations = new ArrayList<>();
-        for (IRestRule rule : activeRules) {
-            if (!rule.getIsActive()) continue;
-            violations.addAll(rule.checkViolation(this.openAPI));
-        }
+        List<Violation> violations = runRuleViolationChecks(activeRules);
+
         //generates Report
         if (generateReport) {
             report.generateReport(violations);
         }
         return violations;
     }
+
+    /**
+     * executes rule checking analysis for provided list of rules. Generates a report file with a specified title.
+     * returns a list of all violations found.
+     * @param activeRules all rules to be checked for
+     * @param title name for the report file
+     * @return list of all violation objects
+     */
+    public List<Violation> runAnalyse(List<IRestRule> activeRules, String title){
+
+        //execute all active Rule Checks
+        List<Violation> violations = runRuleViolationChecks(activeRules);
+
+        //generate Report with custom title
+        report.generateReport(violations,title);
+
+        return violations;
+    }
+
+    /**
+     * executes a Rule check of a provided list of rules
+     * @param activeRules all Rules to be executed
+     * @return list of all Violations for the set of rules
+     */
+    private List<Violation> runRuleViolationChecks (List<IRestRule> activeRules){
+        List<Violation> violations = new ArrayList<>();
+        for (IRestRule rule : activeRules) {
+            if (!rule.getIsActive()) continue;
+            violations.addAll(rule.checkViolation(this.openAPI));
+        }
+        return violations;
+    }
+
 }
