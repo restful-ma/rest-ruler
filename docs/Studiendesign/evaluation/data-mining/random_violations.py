b80acf58da79cf1c0e546ba8324ac3c741c27e7a
manuel_merkel <st155131@stud.uni-stuttgart.de>
2022-11-15 23:15:35 +0100

    docs: add false-positives; add reports and openapi jsons
    Former-commit-id: e8a63d572992e9d76a26d5590362f91a958f3e01 [formerly 39a4908bbc060b82fe0db654564cecf3f3b5a9d2] [formerly f9f15e97a11d278f0a124e369d23f4567795fabc [formerly f1680acdf46a1fce078993c15e54b83b30f0558d]] [formerly 5281be9317de9095b4b0f5362a06d8d69672d462 [formerly 152f7f462ad3046d27ecb7b42ee148e2ca01d093] [formerly 90748578a1b7940cf4e61a4b6701738cea28ef4b [formerly 9bc4b83bec1507c0975d658678f44112c84a967d]]]
    Former-commit-id: 82c07117f3bca448dc83f5547249fbdeca270d00 [formerly 59b071a41acaabd9c3b8f81559b594d7cee135ed] [formerly a9c84a4de17158defa84a7105da6a4782568d4ac [formerly 90748578a1b7940cf4e61a4b6701738cea28ef4b]]
    Former-commit-id: 8580ace5499f154a364a33ee001606b686292546 [formerly 726bef84c18e5b165d01ce208381636cb9e71d52]
    Former-commit-id: 29eddecc5950869fc19b11713a1f755328d49247
    Former-commit-id: ce82cdef3dc415229793c5351fd736eeb7abdcce [formerly 290e2768cbbfbee3afa44a6024e45d7333b3cb18]
    Former-commit-id: 9a3172d6b08c08befbe6a040984386eda611ef95

--- docs/Studiendesign/evaluation/data-mining/random_violations.py
+++ docs/Studiendesign/evaluation/data-mining/random_violations.py
@@ -1,9 +1,9 @@
 import random
 
 # TODO: Set count of rows of violations (with the lines at the beginning)
-max_rows = 0
+max_rows = 159
 # TODO: Set count of violations to be randomly selected
-selcet_rows = 0
+selcet_rows = 30
 
 
 # ------------------ Do not change anything below this line ------------------
@@ -12,7 +12,7 @@ counter = 0
 ran_list = []
 
 while counter != selcet_rows:
-    ran = random.randint(4, max_rows)
+    ran = random.randint(5, max_rows)
 
     if not ran in ran_list:
         ran_list.append(ran)
